6dd26810dd18d1494928e3bb99366de1bd427d3c
Rodrigo Morais <moraisrodrigo@outlook.com>
2026-05-19 17:37:58 -0300

    Grupos estaticos e encapsulamento

---
+++ ConversorDeMoeda/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace ConversorDeMoeda
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            System.Console.WriteLine("Qual é a cotação do dólar?");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            System.Console.WriteLine("Quantos dólares você vai comprar?");
+            double valorDolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            System.Console.WriteLine("Valor a ser pago em reais = " + Conversor.Converter(cotacao, valorDolares));
+        }
+    }
+}
---
+++ MembrosEstaticos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System;
+
+namespace MembrosEstaticos{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            System.Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            System.Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            System.Console.WriteLine("Valor de Pi: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }       
+    }
+}
